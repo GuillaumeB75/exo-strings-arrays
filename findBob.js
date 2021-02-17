b3519f5215ef9ea5282d4761b22bac9e26d0f7e5
GuillaumeB75 <guillaume.bezie@yahoo.com>
2021-02-18 00:14:24 +0100

    fait

---
+++ findBob.js
@@ -0,0 +1,5 @@
+let str = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
+
+let word = 'Bob'
+
+console.log(`The word ${word} ${str.includes('Bob') ? 'is' : 'is not'} in the string`)
